b04b19595b8cf0f46b885701994fe3cbf8e4feeb
Nama Kamu <you@example.com>
2026-07-21 00:43:12 +0700

    kesekian

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\websiteController;
+use App\Http\Controllers\WebsiteController;
 use App\Http\Controllers\DomainController;
 use App\Http\Controllers\LocationController;
+use App\Http\Controllers\CompanyInformationController;
 
 
 Route::post('/extract/website', [WebsiteController::class, 'extract']);
 Route::post('/extract/domain', [DomainController::class, 'extract']);
 Route::post('/extract/location', [LocationController::class, 'extract']);
+Route::post('/company-info', [CompanyInformationController::class, 'show']);    
